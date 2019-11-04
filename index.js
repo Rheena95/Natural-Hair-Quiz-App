5f9fa8bf5e42151e919c5171df287417a081c1a0
Rheena95 <Crhonda39@yahoo.com>
2019-11-04 07:40:40 -0500

    final draft however i need help on my JS

---
+++ index.js
@@ -0,0 +1,58 @@
+const STORE = [
+  {
+    question: "How often should you moisturize natural hair?",
+    answers: [
+      'Weekly',
+      'Daily',
+      'Once a month',
+      'Every 2 weeks'
+    ],
+    correctAnswer:
+      'Daily'
+  },
+  {
+    question:
+      'How often should you trim your natural hair?',
+    answers: [
+      'Once a year',
+      'Every month',
+      'After styling',
+      'Before styling'
+    ],
+    correctAnswer:
+      'Every month'
+  },
+  {
+    question:'Hair is made up of what element?',
+    answers: [
+      'Protein',
+      'Keratin',
+      'Folicite',
+      'Biotin'
+    ],
+    correctAnswer: 'Keratin'
+  },
+  {
+    question: 'What material is used to protect your hair?',
+    answers: [
+      'Cotton',
+      'Silk',
+      'Satin',
+      'Polyester'
+    ],
+    correctAnswer: 'Satin'
+  },
+  {
+    question:
+      'How long does the average human hair grow in a year?',
+    answers: [
+      '12 inches',
+      '3 inches',
+      '18 inches',
+      '6 inches'
+    ],
+    correctAnswer:
+      '6 inches'
+  },
+];
+
