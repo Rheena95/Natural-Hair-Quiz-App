e1d95c4f1b2ce5452e630f7235fb87076db80c1a
Rheena95 <Crhonda39@yahoo.com>
2019-11-05 21:44:48 -0500

    added event listeners

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const STORE = [
-  {
+
+  const quizQuestions = [
+    {
     question: "How often should you moisturize natural hair?",
     answers: [
       'Weekly',
@@ -54,5 +55,80 @@ const STORE = [
     correctAnswer:
       '6 inches'
   },
-];
+    ];
 
+const STORE = {
+    currentView: "home",
+    score: 0,
+    currentQuestion: 0,
+}
+
+function listeners() {
+    $('#start').click(function(){
+        //this function starts the quiz once 'start quiz' is clicked
+        STORE.currentView = "quiz";
+        STORE.currentQuestion = 1;
+        $('.startQuiz').hide();
+        renderQuestion();
+    });
+
+    $('form').submit(function(event){
+        event.preventDefault(); // prevents page from refreshing
+        STORE.currentView = "review";
+        $('button').hide();
+        generateResponse();
+    });
+}
+
+function generateResponse() {
+    // get the selected answer
+    let selectedRadio = $(“input:checked”);
+    let selected = selectedRadio.val(); // grabs the selected/submitted answer
+    
+
+    // check if answer is correct
+    let index = STORE.currentQuestion-1;
+    let correct = selected == quizQuestions[index].correctAnswer;
+    // write response to page
+    let content = '';
+    if(correct) {
+        content += 'Yay! You go girl!';
+    } else {
+        content += "Let's review! What we were looking for is highlighted";
+        selectedRadio.addClass(“rightAnswer”);// lets the person know the correct answer
+    };
+    
+
+
+    $('.js-response').html()
+    // create next button
+
+    
+}
+
+
+function renderQuestion() {
+    let generateHTML = getHTML();
+    $('form').html(generateHTML); // selecting form and replacing the HTML with the passed in value.
+
+}
+
+function getHTML () {
+    let content = `<div>Question ${STORE.currentQuestion} of 5</div>`;
+
+    // add question element
+    let index = STORE.currentQuestion-1; // this var stores the current question index
+    content += `<h2>${quizQuestions[index].question}</h2>`;// adds the question in h2 to content
+    // add radio buttons
+    for(let ind in quizQuestions[index].answers) {
+        let answer = quizQuestions[index].answers[ind]; // returning the string of each answer
+        content += `<input type="radio" name="answer" value="${answer}"> ${answer} <br>`; // this loops throw the answers and creats a radio button
+    }
+    // add submit button
+    content += "<button type='submit'>Submit</button>";
+    return content;
+}
+
+$(function(){
+    listeners();
+});
